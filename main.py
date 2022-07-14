7fc47de2c6fb5ec3151a04480e01758b4359e035
vojtech <p.prusavojta@gmail.com>
2022-07-14 20:05:45 +0200

    Change GUI

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkmacosx import Button
+import customtkinter
 from words import words
 import random
 from resettabletimer import ResettableTimer
@@ -42,32 +42,37 @@ def reset_function():
                                            f"used English words will appear in the top box. At that moment the timer "
                                            f"starts running and you have one minute to type as many words as possible "
                                            f"in the bottom box.\nWhen your time's up, your result will be displayed.",
-                         font=('Helvetica 18 normal'))
+                         font='Helvetica 18 normal')
 
 
-timer = ResettableTimer(60, result_function)
+timer = ResettableTimer(6, result_function)
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 window = Tk()
 window.title("Typing Speed Test")
 window.config(padx=50, pady=50)
 
-canvas = Canvas(window, width=600, height=300, highlightthickness=0, background="white")
-canvas_text = canvas.create_text(20, 10, text=f"Welcome to the typing speed test.\n\nThe average typing speed is around "
-                                           f"40 words per minute. Let's see how fast you are.\nAfter clicking the "
-                                           f"'Start Test' button, 200 random words selected from the 1000 most "
-                                           f"used English words will appear in the top box. At that moment the timer "
-                                           f"starts running and you have one minute to type as many words as possible "
-                                           f"in the bottom box.\nWhen your time's up, your result will be displayed.",
+canvas = Canvas(master=window, width=600, height=300, highlightthickness=0, background="white")
+canvas_text = canvas.create_text(20, 10,
+                                 text=f"Welcome to the typing speed test.\n\nThe average typing speed is around "
+                                      f"40 words per minute. Let's see how fast you are.\nAfter clicking the "
+                                      f"'Start Test' button, 200 random words selected from the 1000 most "
+                                      f"used English words will appear in the top box. At that moment the timer "
+                                      f"starts running and you have one minute to type as many words as possible "
+                                      f"in the bottom box.\nWhen your time's up, your result will be displayed.",
                                  font='Helvetica 18 normal', width=560, anchor="nw")
 canvas.pack()
 
-input_field = Text(window, width=85)
+input_field = Text(master=window, width=85)
 input_field.pack(pady=(20, 20))
 
-start_button = Button(text="Start Test", bg='#77dd77', borderless=1, width=130, command=start_function)
+start_button = customtkinter.CTkButton(master=window, text="Start Test", fg_color='#77dd77', hover_color="#67c267", width=130,
+                                       command=start_function)
 start_button.pack(pady=(0, 20))
 
-reset_button = Button(text="Reset", bg='#FF6666', borderless=1, width=130, command=reset_function)
+reset_button = customtkinter.CTkButton(master=window, text="Reset", fg_color='#FF6666', hover_color="#d95555", width=130, command=reset_function)
 reset_button.pack()
 
 window.mainloop()
